2b4cb78c035894dcca3ddf57f190e3e0e4202a04
Tim Berger <timberger3@gmx.ch>
2026-05-22 09:04:18 +0200

    add more general functions which take the url and import the data

--- code/notebook_utils.py
+++ code/notebook_utils.py
@@ -1,4 +1,4 @@
-## This File contains all the functions for importing and cleaning the data of the züri wie neu project
+## This File contains all the functions for loading the data of the züri wie neu project
 
 import requests
 import geopandas as gpd
@@ -6,6 +6,60 @@ import pandas as pd
 
 
 ### import data
+url_zwn = "https://www.ogd.stadt-zuerich.ch/wfs/geoportal/Zueri_wie_neu?service=WFS&version=1.1.0&request=GetFeature&outputFormat=GeoJSON&typename=zwn_meldungen_p"
+def import_geografic_data(url):
+    """loads the geographic data as a geodataframe
+    
+    This function loads the geografic data from a given url into a geodataframe and converts it the LV95 coordinates
+    if it is successfull it prints 'data loaded successfully' and returns the data,
+    otherwise it prints 'data loading failed'
+    
+    parameters
+    ------------
+    url: a url to a geografic data file that is compatible with gpd.read_file()
+    
+    outputs
+    ------------
+    success: a geodataframe containing the data
+    failed: no output"""
+    
+    try:
+        gdf = gpd.read_file(url).to_crs(epsg="2056")
+        print("data loaded successfully")
+        return gdf
+    except:
+        print( "data loading failed")
+
+
+
+def import_data(url):
+    """imports the data as a dataframe from the given CKAN API URL
+    
+    This function loads the data from a given CKAN API URL into a dataframe
+    if it is successfull it prints 'data loaded successfully' and returns the data,
+    otherwise it prints 'data loading failed'
+    
+    parameters
+    ------------
+    url: a CKAN API URL to a data file
+    
+    outputs
+    ------------
+    success: a dataframe containing the data
+    failed: no output"""
+
+    response_population = requests.get(url)
+    if response_population.status_code == 200:
+        print("data loaded sugessfully")
+
+        #convert the json response into a dictionary (because the CKAN API returns a new dictionary where the data is in result and record)
+        population_dic = response_population.json()["result"]["records"]
+        #convert the dictionary into a dataframe
+        population_df = pd.DataFrame(population_dic)
+        return population_df
+    else:
+        print("data loading failed")
+
 
 ## import züri wie neu data
 
@@ -67,7 +121,7 @@ def import_gastronomy_data ():
 
 def import_population_data():
     """imports the population data as a dataframe from the city of zürich"""
-    url_population = 'https://data.stadt-zuerich.ch/api/3/action/datastore_search'
+    url_population = 'https://data.stadt-zuerich.ch/api/3/action/datastore_search?resource_id=9f9e2f24-96a7-4542-843c-52d44a904110&limit=10000'
     parameter_population = {"resource_id":"9f9e2f24-96a7-4542-843c-52d44a904110", "limit":10000}
     response_population = requests.get(url_population, params=parameter_population)
     if response_population.status_code == 200:
